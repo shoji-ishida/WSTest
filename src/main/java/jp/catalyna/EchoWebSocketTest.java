98aef38065c386fb9d80a9dac9ac4f6d915e4df4
Shoji Ishida <shoji.ishida@gmail.com>
2017-03-10 15:30:24 +0900

    Refactor WebSocketTest to connect to the echo server then wait for echo message. Renamed to EchoWebSocketTest
    Added statistics to HttpMultipleRequests.

---
+++ src/main/java/jp/catalyna/EchoWebSocketTest.java
@@ -0,0 +1,81 @@
+package jp.catalyna;
+
+/**
+ * Created by ishida on 2017/03/07.
+ */
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.AsyncHttpClientConfig;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.ws.WebSocket;
+import org.asynchttpclient.ws.WebSocketTextListener;
+import org.asynchttpclient.ws.WebSocketUpgradeHandler;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// Connect to echo websocket server and wait for echo
+public class EchoWebSocketTest {
+    //private static final String ENDPOINT = "wss://soc.webpush.jp/coins_websocket/push/10000001/74c0575d-6166-458b-81e5-89b0f06b150c/aHR0cDovL2NoZWNrLndlYnB1c2guanAveWFuYXNlL2NvaW5zMi9pbmRleC5odG1s";
+    private static final String ENDPOINT = "ws://echo.websocket.org";
+    //private static final String ENDPOINT = "ws://localhost:8080/websocket";
+
+    private static final Logger log = Logger.getLogger(EchoWebSocketTest.class.getName());
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        // enable trace log for netty and asynchhttpclient
+        //System.setProperty(SimpleLogger.LOG_KEY_PREFIX + "io.netty", "trace");
+        //System.setProperty(SimpleLogger.LOG_KEY_PREFIX + "org.asynchttpclient", "trace");
+
+        AsyncHttpClientConfig config = new DefaultAsyncHttpClientConfig.Builder().setConnectionTtl(-1).setReadTimeout(-1).setRequestTimeout(-1).setConnectTimeout(-1).setKeepAlive(true).build();
+        final Object lock = new Object();
+
+        try (AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient(config)) {
+            final CompletableFuture<WebSocket> future  = asyncHttpClient.prepareGet(ENDPOINT)
+                    .execute(new WebSocketUpgradeHandler.Builder().addWebSocketListener(
+                            new WebSocketTextListener() {
+                                WebSocket ws;
+                                int count;
+
+                                @Override
+                                public void onOpen(WebSocket webSocket) {
+                                    log.log(Level.INFO, "onOpen");
+                                    webSocket.sendMessage("Hello");
+                                    ws = webSocket;
+                                }
+
+                                @Override
+                                public void onClose(WebSocket webSocket) {
+                                    log.log(Level.INFO, "onClose");
+                                }
+
+                                @Override
+                                public void onError(Throwable t) {
+                                    log.log(Level.WARNING, "onError: " + t.getMessage());
+                                }
+
+                                @Override
+                                public void onMessage(String message) {
+                                    log.log(Level.INFO, message);
+                                    if (count++ >= 10) {
+                                        synchronized(lock) {
+                                            lock.notify();
+                                        }
+                                    } else {
+                                        ws.sendMessage("Hi:"+count);
+                                    }
+                                }
+
+
+                            }).build()).toCompletableFuture();
+            WebSocket websocket = future.join();
+            //websocket.sendMessage("Hey");
+            synchronized(lock) {
+                lock.wait();
+            }
+        }
+    }
+}
--- src/main/java/jp/catalyna/HttpMultipleRequests.java
+++ src/main/java/jp/catalyna/HttpMultipleRequests.java
@@ -3,6 +3,7 @@ package jp.catalyna;
 /**
  * Created by ishida on 2017/03/07.
  */
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.Response;
 
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -19,15 +21,18 @@ import java.util.stream.Stream;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
+// Make multiple Http requests concurrently and wait for response asynchronously
 public class HttpMultipleRequests {
     private static final String ENDPOINT = "http://yanase-W331AU.local:8080/SpringTest_war/spring";
 
-    private static final int NUM_OF_REQUESTS = 100;
+    private static final int NUM_OF_REQUESTS = 1000;
     private static final Logger log = Logger.getLogger(HttpMultipleRequests.class.getName());
+    private static final AtomicInteger success = new AtomicInteger(0);
 
     public static void main(String[] args) throws IOException {
-
+        long startTime;
         try(AsyncHttpClient client = asyncHttpClient()) {
+            startTime = System.currentTimeMillis();
             // Generates NUM_OF_REQUESTS of CompletableFuture from AsyncHttpClient
             List<CompletableFuture<Response>> futures = Stream.generate(() -> client.prepareGet(ENDPOINT).execute().toCompletableFuture()).parallel().limit(NUM_OF_REQUESTS).collect(Collectors.toList());
 
@@ -37,8 +42,18 @@ public class HttpMultipleRequests {
 
             // Process Response from each Future
             futures.parallelStream().forEach(future -> {
-                future.thenApplyAsync(Response::getStatusText).thenAcceptAsync(System.out::println);
+                //future.thenApplyAsync(Response::getStatusText).thenAcceptAsync(System.out::println);
+                future.thenApplyAsync(Response::getStatusCode).thenAcceptAsync(HttpMultipleRequests::count);
             });
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Took: " + DurationFormatUtils.formatPeriod(startTime, endTime, "mm:ss.SSS"));
+        System.out.println("Ok: " + success + " Err: " + Integer.toString(NUM_OF_REQUESTS - success.get()));
+    }
+
+    private static void count(Integer status) {
+        if (status == 200) {
+            success.incrementAndGet();
         }
     }
 }
