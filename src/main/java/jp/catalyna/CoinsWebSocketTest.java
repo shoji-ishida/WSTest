78a63a65b1a35ff6dae96ca28a4c1bfa64e94d60
Shoji Ishida <shoji.ishida@gmail.com>
2017-03-22 19:23:27 +0900

    implemented sendDisplay and sendClick

--- src/main/java/jp/catalyna/CoinsWebSocketTest.java
+++ src/main/java/jp/catalyna/CoinsWebSocketTest.java
@@ -3,6 +3,10 @@ package jp.catalyna;
 /**
  * Created by ishida on 2017/03/07.
  */
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import org.apache.commons.cli.*;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.asynchttpclient.*;
@@ -11,8 +15,11 @@ import org.asynchttpclient.ws.WebSocketTextListener;
 import org.asynchttpclient.ws.WebSocketUpgradeHandler;
 import org.slf4j.impl.SimpleLogger;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -28,6 +35,15 @@ import static org.asynchttpclient.Dsl.asyncHttpClient;
 // Connect to echo websocket server and wait for echo
 public class CoinsWebSocketTest {
     private static final String ENDPOINT = "wss://soc.webpush.jp/coins_websocket/push/10000001/74c0575d-6166-458b-81e5-89b0f06b150c/aHR0cDovL2NoZWNrLndlYnB1c2guanAveWFuYXNlL2NvaW5zMi9pbmRleC5odG1s";
+
+    private static final String MESSAGE_TYPE = "type";
+    private static final String MESSAGE_TYPE_DISPLAY = "display";
+    private static final String MESSAGE_TYPE_CLICK = "click";
+    private static final String MESSAGE_ID = "messageId";
+
+    private static final String TIME_FORMAT = "mm:ss.SSS";
+
+
 
     private static final int NUM_OF_SESSIONS = 1000;
     private static final Logger log = Logger.getLogger(CoinsWebSocketTest.class.getName());
@@ -53,7 +69,8 @@ public class CoinsWebSocketTest {
 
         options.addOption(Option.builder("r").hasArg(false).required(false).desc("Generate random UUID for URL parameter").build());
         options.addOption(Option.builder("closeOnOpen").hasArg(false).required(false).desc("Close websocket connection when onOpen is called").build());
-        options.addOption(Option.builder("click").hasArg(false).required(false).desc("Send click message on onMassage").build());
+        options.addOption(Option.builder("sendDisplay").hasArg(false).required(false).desc("Send display message on onMassage").build());
+        options.addOption(Option.builder("sendClick").hasArg(false).required(false).desc("Send click message on onMassage").build());
         CommandLineParser parser = new DefaultParser();
 
         int c = NUM_OF_SESSIONS;
@@ -78,6 +95,8 @@ public class CoinsWebSocketTest {
                 isRandomUuid = true;
             }
             final boolean isCloseOnOpen = cmd.hasOption("closeOnOpen");
+            final boolean isSendDisplay = cmd.hasOption("sendDisplay");
+            final boolean isSendClick = cmd.hasOption("sendClick");
 
             String[] leftArgs = cmd.getArgs();
             String endpoint = leftArgs[0];
@@ -128,8 +147,8 @@ public class CoinsWebSocketTest {
                                         if (concurrentCount == concurrent) {
                                             concurrent_count.set(0);
                                             long endTime = System.currentTimeMillis();
-                                            String duration = DurationFormatUtils.formatPeriod(startTime, endTime, "mm:ss.SSS");
-                                            log.log(Level.INFO, "Completed " + sessionCount + " requests (" + duration +")");
+                                            String duration = DurationFormatUtils.formatPeriod(startTime, endTime, TIME_FORMAT);
+                                            log.log(Level.INFO, "Completed " + sessionCount + " requests. (" + duration +")");
                                             synchronized (concurrencyLock) {
                                                 concurrencyLock.notify();
                                             }
@@ -158,10 +177,29 @@ public class CoinsWebSocketTest {
 
                                     @Override
                                     public void onMessage(String message) {
+                                        // respond with display/click message
+                                        if (isSendClick && isSendDisplay) {
+                                            String messageId = parseMessageId(message);
+                                            // display
+                                            if (isSendDisplay) {
+                                                String jsonString = createJson(MESSAGE_TYPE_DISPLAY, messageId);
+                                                sendMessage(ws, jsonString);
+                                            }
+                                            // click
+                                            if (isSendClick) {
+                                                String jsonString = createJson(MESSAGE_TYPE_CLICK, messageId);
+                                                sendMessage(ws, jsonString);
+                                            }
+                                        }
                                         //log.log(Level.INFO, message);
                                         int count = push_count.incrementAndGet();
+                                        if (count == 0) {
+                                            startTime = System.currentTimeMillis();
+                                        }
                                         if (count == session_count.get()) {
-                                            log.log(Level.INFO, count + " push(es) received.");
+                                            long endTime = System.currentTimeMillis();
+                                            String duration = DurationFormatUtils.formatPeriod(startTime, endTime, TIME_FORMAT);
+                                            log.log(Level.INFO, count + " push(es) received. (" + duration + ")");
                                             synchronized (lock) {
                                                 lock.notify();
                                             }
@@ -171,6 +209,7 @@ public class CoinsWebSocketTest {
 
                                 }).build()).toCompletableFuture();
             }
+
             synchronized(lock) {
                 //log.log(Level.INFO, "wait on lock");
                 lock.wait();
@@ -200,5 +239,50 @@ public class CoinsWebSocketTest {
         }
     }
 
+    private static JsonFactory jsonFactory = new JsonFactory();
+    private static String parseMessageId(String message) {
+        String messageId = null;
+        try {
+            JsonParser parser = jsonFactory.createParser(message);
+            if (parser.nextToken() != JsonToken.START_OBJECT) {
+                throw new IOException("Expected data to start with an Object");
+            }
+            while (parser.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = parser.getCurrentName();
+                parser.nextToken();
+                if (fieldName.equals(MESSAGE_ID)) {
+                    messageId = parser.getValueAsString();
+                    break;
+                }
+            }
+            parser.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return messageId;
+    }
 
+    private static String createJson(String type, String messageId) {
+        String jsonString = null;
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            JsonGenerator generator = jsonFactory.createGenerator(bos);
+            generator.writeStartObject();
+            generator.writeStringField(MESSAGE_TYPE, type);
+            generator.writeStringField(MESSAGE_ID, messageId);
+            generator.writeEndObject();
+            generator.close();
+            jsonString = bos.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
+    private static void sendMessage(WebSocket webSocket, String message) {
+        webSocket.sendMessage(message);
+    }
+
+    //private static String testMessage = "{\"type\":\"show\",\"messageId\":\"10000034\",\"messageType\":\"1\"}";
 }
